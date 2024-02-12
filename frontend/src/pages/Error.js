6ea56ea07d2422e9474072aaa2d7df659cfd1445
PunithRajKumarM <punithrajkumar.m@celestialsys.com>
2024-02-12 16:00:48 +0530

    Initial commit

---
+++ frontend/src/pages/Error.js
@@ -0,0 +1,25 @@
+import React from "react";
+import PageContent from "../components/PageContent";
+import { useRouteError } from "react-router-dom";
+import MainNavigation from "../components/MainNavigation";
+
+export default function ErrorPage() {
+  const error = useRouteError();
+  let title = "An error occured";
+  let message = "Something went wrong!";
+  if (error.status === 500) {
+    message = error.data.message;
+  }
+  if (error.status === 400) {
+    title = "Not found!";
+    message = "Could not find resource or page";
+  }
+  return (
+    <>
+      <MainNavigation />
+      <PageContent title={title}>
+        <p>{message}</p>
+      </PageContent>
+    </>
+  );
+}
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,13 @@
+import PageContent from "../components/PageContent";
+
+function HomePage() {
+  return (
+    <>
+      <PageContent title="Welcome!">
+        <p>Browse all amazing events!</p>
+      </PageContent>
+    </>
+  );
+}
+
+export default HomePage;
